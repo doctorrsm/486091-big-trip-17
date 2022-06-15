89c8f9dc1dd210a087f90a4842a16f7adcb2aee6
Sergey Rutkovsky <dr.bibelman@gmail.com>
2022-06-15 07:05:13 +0300

    критерий д15 - упрощает вовзращаение результата функций

--- src/points-api-service.js
+++ src/points-api-service.js
@@ -30,9 +30,7 @@ export default class PointsApiService extends ApiService {
       body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
-    const parsedResponse = await ApiService.parseResponse(response);
-
-    return parsedResponse;
+    return await ApiService.parseResponse(response);
   };
 
   addPoint = async (point) => {
@@ -43,19 +41,13 @@ export default class PointsApiService extends ApiService {
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
-    const parsedResponse = await ApiService.parseResponse(response);
-
-    return parsedResponse;
+    return await ApiService.parseResponse(response);
   };
 
-  deletePoint = async (point) => {
-    const response = await this._load({
-      url: `points/${point.id}`,
-      method: Method.DELETE,
-    });
-
-    return response;
-  };
+  deletePoint = async (point) => await this._load({
+    url: `points/${point.id}`,
+    method: Method.DELETE,
+  });
 
   #adaptToServer = (point) => {
 
